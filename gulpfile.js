35d9b9dd5714f12c6f939eb9044550cc9dfa1c66
rajeshwarpatlolla <rajeshwar.patlolla@gmail.com>
2015-04-28 16:08:48 +0530

    v0.1.2 released

--- gulpfile.js
+++ gulpfile.js
@@ -8,31 +8,31 @@ var minifyHtml = require("gulp-minify-html");
 var minifycss = require("gulp-minify-css");
 
 gulp.task('html2js', function () {
-    gulp.src(['./src/*.html'])
-        .pipe(minifyHtml())
-        .pipe(ngHtml2Js({
-            moduleName: "ionic-timepicker.templates"
-        }))
-        .pipe(concat("templates.js"))
-        .pipe(uglify())
-        .pipe(gulp.dest("./dist"));
+  gulp.src(['./src/*.html'])
+    .pipe(minifyHtml())
+    .pipe(ngHtml2Js({
+      moduleName: "ionic-timepicker.templates"
+    }))
+    .pipe(concat("templates.js"))
+    .pipe(uglify())
+    .pipe(gulp.dest("./dist"));
 });
 
-gulp.task('build',['html2js','cssminify'], function () {
-    gulp.src(['./src/ionic-timepicker.js'])
-        .pipe(uglify())
-        .pipe(gulp.dest("./dist"));
+gulp.task('build', ['html2js', 'cssminify'], function () {
+  gulp.src(['./src/ionic-timepicker.js'])
+    .pipe(uglify())
+    .pipe(gulp.dest("./dist"));
 });
 
 gulp.task('cssminify', function () {
-    return gulp.src('./src/*.css')
-        .pipe(minifycss())
-        .pipe(gulp.dest('./dist'));
+  return gulp.src('./src/*.css')
+    .pipe(minifycss())
+    .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('clean', function () {
-    return gulp.src('dist', {read: false})
-        .pipe(clean());
+  return gulp.src('dist', {read: false})
+    .pipe(clean());
 });
 
-gulp.task('default',['clean','build']);
+gulp.task('default', ['clean', 'build']);
